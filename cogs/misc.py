88968f450acbb5bd82bef08992f28fc05c8f94e8
xshadowsexy <82493057+Sweety187@users.noreply.github.com>
2021-07-23 20:06:27 +0700

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -14,21 +14,23 @@ class misc(commands.Cog):
 	@commands.command()
 	async def help(self, ctx, page=0):
 		if not page or page > 4:
-			default = '`1.` Модерация\n`2.` Музыка\n`3.` Утилиты\n`4.` Прочее\n\nИспользуйте `!!help [page]` для просмотра списка команд из этой категории. Что нового: `!!about`. [Сервер поддержки](https://discord.gg/fj4vCdFVw3).'
+			default = '`1.` Модерация\n`2.` Музыка\n`3.` Утилиты\n`4.` Прочее\n\nИспользуйте `!!help [page]` для просмотра списка команд из этой категории.'
 			e = discord.Embed()
 			e.add_field(name='Доступные категории команд', value=default)
-			e.set_footer(text='© 2021 Sweety187 | Все права защищены.', icon_url='https://media.discordapp.net/attachments/832662675963510827/855762014010081300/b5222c5b.jpg')
+			e.set_footer(text='© 2021 Sweety187 | Все права защищены.',
+				     icon_url='https://media.discordapp.net/attachments/832662675963510827/855762014010081300/b5222c5b.jpg')
 			await ctx.send(embed=e)
 			return
 		mod = '`!!kick <member> [reason]` - кикнуть пользователя\n`!!ban <member> [reason]` - забанить пользователя\n`!!unban <user>` - разбанить пользователя\n`!!mute <member> [time] [reason]` - замутить пользователя\n`!!unmute <member>` - размутить пользователя\n`!!clear <amount>` - удалить последние N сообщений в канале'
 		music = '`!!join [channel]` - присоединиться к голосовому каналу\n`!!leave` - покуинуть голосовой канал\n`!!play <query>` - воспроизвести музыку с YouTube\n`!!radio <stream>` - проигрывать радио в голосовом канале\n`!!stop` - остановить воспроизведение\n`!!pause` - приостановить воспроизведение\n`!!resume` - продолжить воспроизведение\n`!!repeat` - зациклить воспроизведение\n`!!now` - узнать, что сейчас играет'
 		utils = '`!!avatar [member]` - вывести аватар пользователя\n`!!yt <query>` - найти видео на YouTube\n`!!ping <ip>` - выводит информацию о сервере Minecraft\n`!!2b2t` - выводит данные о сервере 2b2t (очередь и т.п.)\n`!!skin <nick>` - выводит скин игрока Minecraft\n`!!say <text>` - отправить сообщение от имени бота\n`!!embed <text>` - отправить ваш текст внутри ембеда\n`!!timer <time>` - поставить таймер\n`!!user [user]` - информация о пользователе\n`!!server` - информация о сервере'
-		misc = '`!!neko` - случайная картинка с неко\n`!!nekogif` - случайная гифка с неко\n`!!cat` - случайная картинка с котом\n`!!nsfw [tag]` - хентай-картинка по тегу («lewd», если тег не указан)\n`!!invite` - добавить меня на свой сервер\n`!!about` - сведения о текущей версии бота\n`!!status` - статистика бота\n`!!bug <report>` - сообщить об ошибке'
+		misc = '`!!neko` - случайная картинка с неко\n`!!nekogif` - случайная гифка с неко\n`!!cat` - случайная картинка с котом\n`!!nsfw [tag]` - хентай-картинка по тегу («lewd», если тег не указан)\n`!!invite` - добавить меня на свой сервер\n`!!about` - сведения о текущей версии бота\n`!!status` - статистика бота'
 		pages = [mod, music, utils, misc]
 		titles = ['1. Модерация', '2. Музыка', '3. Утилиты', '4. Прочее']
 		e = discord.Embed()
 		e.add_field(name=titles[page - 1], value=pages[page - 1])
-		e.set_footer(text='© 2021 Sweety187 | Все права защищены.', icon_url='https://media.discordapp.net/attachments/832662675963510827/855762014010081300/b5222c5b.jpg')
+		e.set_footer(text='© 2021 Sweety187 | Все права защищены.',
+			     icon_url='https://media.discordapp.net/attachments/832662675963510827/855762014010081300/b5222c5b.jpg')
 		await ctx.send(embed=e)
 
 	@commands.command()
@@ -37,7 +39,7 @@ class misc(commands.Cog):
 		uptime = datetime.timedelta(seconds=uptime)
 		e = discord.Embed(title="Статистика бота")
 		e.add_field(name='Аптайм', value=uptime, inline=True)
-		e.add_field(name='Версия', value='s1.0.0', inline=True)
+		e.add_field(name='Версия', value='s1.0.1', inline=True)
 		e.add_field(name='Серверов', value=len(self.bot.guilds), inline=True)
 		users = 0
 		for guild in self.bot.guilds:
@@ -48,21 +50,22 @@ class misc(commands.Cog):
 					inline=True)
 		e.set_thumbnail(url="https://media.discordapp.net/attachments/832662675963510827/857631236355522650/logo.png")
 		e.set_footer(text='© 2021 Sweety187 | Все права защищены.',
-					 icon_url='https://media.discordapp.net/attachments/832662675963510827/855762014010081300/b5222c5b.jpg')
+			     icon_url='https://media.discordapp.net/attachments/832662675963510827/855762014010081300/b5222c5b.jpg')
 		await ctx.send(embed=e)
 
 	@commands.command()
 	async def about(self, ctx):
-		e=discord.Embed(title="Стабильная 1.0.0 от 11.07.2021 14:27 MSK")
-		fixed="ㆍИсправлены почти все возможные ошибки."
+		e=discord.Embed(title="Стабильная 1.0.1 от 23.07.2021 16:04 MSK")
+		fixed="ㆍИсправления мелких ошибок."
 		improved="ㆍИзменен интерфейс бота."
 		added="ㆍДобавлена возможность указывать причину для бана, кика, мута."
-		deleted="ㆍ."
+		deleted="ㆍКоманда `!!bug`, вместе с сервером поддержки."
 		e.add_field(name='Исправлено', value=fixed, inline=False)
-		e.add_field(name='Изменено', value=improved, inline=False)
-		e.add_field(name='Добавлено', value=added, inline=False)
-		#emb.add_field(name='Удалено', value=deleted, inline=False)
-		e.set_footer(text='© 2021 Sweety187 | Все права защищены.', icon_url = 'https://media.discordapp.net/attachments/832662675963510827/855762014010081300/b5222c5b.jpg')
+		#e.add_field(name='Изменено', value=improved, inline=False)
+		#e.add_field(name='Добавлено', value=added, inline=False)
+		emb.add_field(name='Удалено', value=deleted, inline=False)
+		e.set_footer(text='© 2021 Sweety187 | Все права защищены.',
+			     icon_url='https://media.discordapp.net/attachments/832662675963510827/855762014010081300/b5222c5b.jpg')
 		await ctx.send(embed=e)
 
 	@commands.command()
@@ -70,8 +73,8 @@ class misc(commands.Cog):
 	async def nsfw(self, ctx, req='lewd'):
 		possible = ['feet', 'yuri', 'trap', 'futanari', 'hololewd', 'lewdkemo', 'solog',
 					'feetg', 'cum', 'erokemo', 'les', 'lewdk', 'ngif', 'lewd', 'gecg',
-					'eroyuri', 'eron', 'cum_jpg', 'bj', 'nsfw_neko_gif', 'solo', 'kemonomimi',
-					'anal', 'hentai', 'erofeet', 'keta', 'blowjob', 'pussy', 'tits', 'holoero',
+					'eroyuri', 'eron', 'cum_jpg', 'bj', 'nsfw_neko_gif', 'solo', 'anal',
+			    		'hentai', 'erofeet', 'keta', 'blowjob', 'pussy', 'tits', 'holoero',
 					'pussy_jpg', 'pwankg', 'classic', 'kuni', 'femdom', 'spank', 'erok', 'boobs',
 					'random_hentai_gif', 'smallboobs', 'ero']
 		if req not in possible:
@@ -105,18 +108,6 @@ class misc(commands.Cog):
 		e = discord.Embed()
 		e.set_image(url=link)
 		await ctx.send(embed=e)
-
-	@commands.command()
-	@commands.cooldown(1, 1800, commands.BucketType.guild)
-	async def bug(self, ctx, *, message):
-		guild = discord.utils.get(self.bot.guilds, id=859735750555992064)
-		channel = discord.utils.get(guild.channels, id=859838257542201365)
-		e = discord.Embed(title="Сообщение об ошибке")
-		e.add_field(name="Автор", value=ctx.author, inline=False)
-		e.add_field(name="Сообщение", value=message, inline=False)
-		e.set_thumbnail(url="https://www.iconsdb.com/icons/download/white/error-6-64.png")
-		await channel.send(embed=e)
-		await success(ctx, 'Ваше сообщение было отправлено на [сервер поддержки](https://discord.gg/fj4vCdFVw3).')
 
 	@commands.command()
 	async def invite(self, ctx):
