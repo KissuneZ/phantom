97ca22e57a3a924e0d3a7c891824d1226639f527
xshadowsexy <82493057+Sweety187@users.noreply.github.com>
2021-08-02 20:05:00 +0700

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -42,7 +42,7 @@ class misc(commands.Cog):
 					 icon_url='https://media.discordapp.net/attachments/832662675963510827/855762014010081300/b5222c5b.jpg')
 		await ctx.send(embed=e)
 
-	@commands.command()
+	@commands.command(aliases=['botinfo'])
 	async def status(self, ctx):
 		uptime = int(time.time() - nullTime)
 		uptime = datetime.timedelta(seconds=uptime)
@@ -55,12 +55,11 @@ class misc(commands.Cog):
 			users += guild.member_count
 		e.add_field(name='Пользователей', value=users, inline=True)
 		e.add_field(name='Нагрузка',
-					value=f'ЦП: {psutil.cpu_percent()}% ОЗУ: {psutil.virtual_memory().percent}%',
-					inline=True)
-		e.set_thumbnail(
-			url="https://media.discordapp.net/attachments/832662675963510827/857631236355522650/logo.png")
+			    value=f'ЦП: {psutil.cpu_percent()}% ОЗУ: {psutil.virtual_memory().percent}%',
+			    inline=True)
+		e.set_thumbnail(url="https://media.discordapp.net/attachments/832662675963510827/857631236355522650/logo.png")
 		e.set_footer(text='© 2021 Sweety187 | Все права защищены.',
-					 icon_url='https://media.discordapp.net/attachments/832662675963510827/855762014010081300/b5222c5b.jpg')
+			     icon_url='https://media.discordapp.net/attachments/832662675963510827/855762014010081300/b5222c5b.jpg')
 		await ctx.send(embed=e)
 
 	@commands.command()
